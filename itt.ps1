c214db61a60a8057a46d7b60d9797d897cd5628b
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-10 09:18:34 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -11458,11 +11458,14 @@ $itt.event.FindName('closebtn').add_MouseLeftButtonDown({ $itt.event.Close() })
 $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({ DisablePopup; $itt.event.Close() })
 $itt.event.FindName('title').text = 'Changelog'.Trim()
 $itt.event.FindName('date').text = '01/31/2025'.Trim()
+$itt.event.FindName('ytv').add_MouseLeftButtonDown({
+Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
+})
 $itt.event.FindName('esg').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
 })
-$itt.event.FindName('shell').add_MouseLeftButtonDown({
-Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
+$itt.event.FindName('preview').add_MouseLeftButtonDown({
+Start-Process('https://github.com/emadadel4/itt')
 })
 $itt.event.FindName('preview2').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
@@ -11470,11 +11473,8 @@ Start-Process('https://github.com/emadadel4/itt')
 $itt.event.FindName('ps').add_MouseLeftButtonDown({
 Start-Process('https://www.palestinercs.org/en/Donation')
 })
-$itt.event.FindName('ytv').add_MouseLeftButtonDown({
-Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
-})
-$itt.event.FindName('preview').add_MouseLeftButtonDown({
-Start-Process('https://github.com/emadadel4/itt')
+$itt.event.FindName('shell').add_MouseLeftButtonDown({
+Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
 })
 $itt.event.Add_PreViewKeyDown({
 if ($_.Key -eq "Escape") { $itt.event.Close() }
@@ -11922,8 +11922,8 @@ if ($selectedItem) {
 $checkBox = $selectedItem.Children[0].Children[0]
 if ($checkBox) {
 $checkBox.IsChecked = -not $checkBox.IsChecked
-if (-not ($global:CheckedItems -is [System.Collections.ArrayList])) {
-$global:CheckedItems = New-Object System.Collections.ArrayList
+if (-not $global:CheckedItems) {
+$global:CheckedItems = [System.Collections.ArrayList]::new()
 }
 if ($checkBox.IsChecked) {
 if (-not ($global:CheckedItems | Where-Object { $_.Content -eq $checkBox.Content })) {
