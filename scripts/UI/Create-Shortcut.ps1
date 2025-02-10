f9d9217e895fb491765cad70072f98843cbbe0bb
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-10 06:47:09 +0200

    Update Create-Shortcut.ps1

--- scripts/UI/Create-Shortcut.ps1
+++ scripts/UI/Create-Shortcut.ps1
@@ -1,27 +1,7 @@
 function ITTShortcut {
-
-    <#
-        .SYNOPSIS
-            Creates a desktop shortcut.
-        .DESCRIPTION
-            The `ITTShortcut` function creates a shortcut on the user's desktop that points to a PowerShell executable with a specified command.
-            It downloads a custom icon from a specified URL, saves it to the `C:\ProgramData\itt\` folder, and sets this icon for the shortcut.
-            The PowerShell script specified in the shortcut executes a command to run a script from a provided URL.
-    #>
-
-    # URL of the icon file
-    # Determine the path in AppData\Roaming
-    $appDataPath = "$env:ProgramData/itt"
-    $localIconPath = Join-Path -Path $appDataPath -ChildPath "itt.ico"
-    # Download the icon file
-    Invoke-WebRequest -Uri $itt.icon -OutFile $localIconPath
-    # Create a shortcut object
+    $iconPath = "$env:ProgramData\itt\itt.ico"
+    Invoke-WebRequest -Uri $itt.icon -OutFile $iconPath
     $Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("$([Environment]::GetFolderPath('Desktop'))\ITT Emad Adel.lnk")
-    # Set the target path to PowerShell with your command
-    $Shortcut.TargetPath = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
-    $Shortcut.Arguments = "-ExecutionPolicy Bypass -Command ""irm bit.ly/ittea | iex"""
-    # Set the icon path to the downloaded icon file in AppData\Roaming
-    $Shortcut.IconLocation = "$localIconPath"
-    # Save the shortcut
+    $Shortcut.TargetPath, $Shortcut.Arguments, $Shortcut.IconLocation = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe", "-ExecutionPolicy Bypass -Command ""irm bit.ly/ittea | iex""", $iconPath
     $Shortcut.Save()
 }
