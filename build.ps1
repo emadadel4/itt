61e8e9b4b89f27535c733e30cb8b7a4910ac5093
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-10 03:28:31 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -113,7 +113,28 @@ function GenerateCheckboxes {
         # Clean description and category to remove special characters
         $CleanedDescription = $Item.Description -replace '[^\w\s./]', ''
         $CleanedCategory = $Item.Category -replace '[^\w\s]', ''
-        $CleanedName = $Item.Name -replace '[^a-zA-Z0-9]', ''
+        $CleanedName = $Item.Name -replace '[^a-zA-Z]', ''
+
+
+
+        $CleanedName = $Item.Name -replace '[^a-zA-Z]', ''  # تنظيف النص
+        $Chars = $CleanedName.ToCharArray()
+
+        # تنفيذ خوارزمية Fisher-Yates Shuffle للخلط العشوائي
+        for ($i = $Chars.Length - 1; $i -gt 0; $i--) {
+            $j = Get-Random -Minimum 0 -Maximum ($i + 1)
+            $temp = $Chars[$i]
+            $Chars[$i] = $Chars[$j]
+            $Chars[$j] = $temp
+        }
+
+        $ShuffledName = -join $Chars
+        Write-Host $ShuffledName
+
+
+
+        
+
         $Content = $Item.$ContentField
 
         # Optional attributes for CheckBox based on fields
@@ -121,16 +142,28 @@ function GenerateCheckboxes {
 
         $Tips = if ($TipsField) { "ToolTip=`"Install it again to update. If there is an issue with the program, please report the problem on the GitHub repository.`"" } else { "" }
 
-        $Name = if ($NameField) { "Name=`"$($CleanedName)`"" } else { "" }
+
+
+
+        $Name = if ($NameField -eq "Random") { 
+            
+            "Name=`"$($ShuffledName)`"" 
+        } 
+        elseif($NameField -eq "Name")
+        {
+            "Name=`"$($CleanedName)`"" 
+        }elseif ($NameField -eq "None") {
+             
+        }
 
         $Toggle = if ($ToggleField) { "Style=`"{StaticResource ToggleSwitchStyle}`"" } else { "" }
-        $IsChecked = if ($IsCheckedField) { "IsChecked=`"$($Item.$IsCheckedField)`"" } else { "" }
+        $IsChecked = if ($IsCheckedField) { "IsChecked=`"false`"" } else { "" }
 
         # Build the CheckBox and its container
         $Checkboxes += @"
         <StackPanel Orientation="Vertical" Margin="10">
             <StackPanel Orientation="Horizontal">
-                <CheckBox Content="$Content" $Tag $IsChecked $Toggle $Name $Tips FontWeight="SemiBold" FontSize="15" Foreground="{DynamicResource TextColorSecondaryColor}" HorizontalAlignment="Center" VerticalAlignment="Center"/>
+                <CheckBox Content="$Content" $Name $IsChecked $Tag $Toggle $Tips FontWeight="SemiBold" FontSize="15" Foreground="{DynamicResource TextColorSecondaryColor}" HorizontalAlignment="Center" VerticalAlignment="Center"/>
                 <Label HorizontalAlignment="Center" VerticalAlignment="Center" Margin="5,0,0,0" FontSize="13" Content="$CleanedCategory"/>
             </StackPanel>
             <TextBlock Width="666" Background="Transparent" Margin="8" Foreground="{DynamicResource TextColorSecondaryColor2}" FontSize="15" FontWeight="SemiBold" VerticalAlignment="Center" TextWrapping="Wrap" Text="$CleanedDescription."/>
@@ -683,7 +716,7 @@ try {
     catch {
         Write-Error "An error occurred while processing the XAML content: $($_.Exception.Message)"
     }
-    $AppsCheckboxes = GenerateCheckboxes -Items $itt.database.Applications -ContentField "Name" -TagField "Category" -IsCheckedField "check" -TipsField "show"
+    $AppsCheckboxes = GenerateCheckboxes -Items $itt.database.Applications -ContentField "Name" -NameField "None" -TagField "Category" -IsCheckedField "check" -TipsField "show"
     $TweaksCheckboxes = GenerateCheckboxes -Items $itt.database.Tweaks -ContentField "Name" -TagField "Category" -IsCheckedField "check"
     $SettingsCheckboxes = GenerateCheckboxes -Items $itt.database.Settings -ContentField "Name" -NameField "Name" -ToggleField "Style=" { StaticResource ToggleSwitchStyle }""
     $MainXamlContent = $MainXamlContent -replace "{{Apps}}", $AppsCheckboxes 
