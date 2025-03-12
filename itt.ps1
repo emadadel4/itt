9f8b114b6831bb7efe93ed8cc2ee94ac94afaa91
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-03-12 05:10:42 +0200

    Update Install-Winget.ps1

--- itt.ps1
+++ itt.ps1
@@ -7445,40 +7445,8 @@ function Install-ITTaChoco {
 }
 function Install-Winget {
 
-    <#
-        .SYNOPSIS
-        Installs Winget on Windows systems that support it.
-
-        .DESCRIPTION
-        This function checks if Winget is installed on the system. If not, it verifies the system's architecture and Windows version to ensure compatibility. It 
-        then downloads and installs the necessary dependencies and Winget itself.
-    #>
-
-    if(Get-Command winget -ErrorAction SilentlyContinue) {return}
-    $ComputerInfo = Get-ComputerInfo -ErrorAction Stop
-    $arch = [int](($ComputerInfo).OsArchitecture -replace '\D', '')
-
-    if ($ComputerInfo.WindowsVersion -lt "1809") {
-        Add-Log -Message "Winget is not supported on this version of Windows Upgrade to 1809 or newer." -Level "info" 
-        return
-    }
-
-    $VCLibs = "https://aka.ms/Microsoft.VCLibs.x$arch.14.00.Desktop.appx"
-    $UIXaml = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x$arch.appx"
-    $WingetLatset = "https://aka.ms/getwinget"
-
     try {
-        
-        Add-Log -Message "Installing Winget... This might take several minutes" -Level "info"
-        Start-BitsTransfer -Source $VCLibs -Destination "$env:TEMP\Microsoft.VCLibs.Desktop.appx"
-        Start-BitsTransfer -Source $UIXaml -Destination "$env:TEMP\Microsoft.UI.Xaml.appx"
-        Start-BitsTransfer -Source $WingetLatset -Destination "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-
-        Add-AppxPackage "$env:TEMP\Microsoft.VCLibs.Desktop.appx"
-        Add-AppxPackage "$env:TEMP\Microsoft.UI.Xaml.appx"
-        Add-AppxPackage "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-        Start-Sleep -Seconds 1
-        Add-Log -Message "Successfully installed Winget. Continuing to install selected apps..." -Level "info"
+        Start-Process -FilePath "itt" -ArgumentList "install winget y-" -NoNewWindow -Wait -PassThru
         return
     }
     catch {
@@ -13706,13 +13674,18 @@ function Show-Event {
         $itt.event.FindName('date').text = '03/01/2025'.Trim()
         
     
-            $itt.event.FindName('shell').add_MouseLeftButtonDown({
-                    Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
+            $itt.event.FindName('ps').add_MouseLeftButtonDown({
+                    Start-Process('https://www.palestinercs.org/en/Donation')
                 })
             
             
-            $itt.event.FindName('preview').add_MouseLeftButtonDown({
-                    Start-Process('https://github.com/emadadel4/itt')
+            $itt.event.FindName('ytv').add_MouseLeftButtonDown({
+                    Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
+                })
+            
+            
+            $itt.event.FindName('shell').add_MouseLeftButtonDown({
+                    Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
                 })
             
             
@@ -13726,13 +13699,8 @@ function Show-Event {
                 })
             
             
-            $itt.event.FindName('ps').add_MouseLeftButtonDown({
-                    Start-Process('https://www.palestinercs.org/en/Donation')
-                })
-            
-            
-            $itt.event.FindName('ytv').add_MouseLeftButtonDown({
-                    Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
+            $itt.event.FindName('preview').add_MouseLeftButtonDown({
+                    Start-Process('https://github.com/emadadel4/itt')
                 })
             
             
