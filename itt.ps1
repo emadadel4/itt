58839f75303518c50c461ef0a65f6261af42feb7
ITT Community <19177373+emadadel4@users.noreply.github.com>
2025-06-22 11:38:16 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -471,91 +471,6 @@ function ExecuteCommand {
         Add-Log -Message "The specified command was not found." -Level "WARNING"
     }
 }
-function Finish {
-    
-    <#
-        .SYNOPSIS
-        Clears checkboxes in a specified ListView and displays a notification.
-        .DESCRIPTION
-        Clears all checkboxes in the ListView named "myListView" and displays a notification with the title "Process Completed", message "All items have been processed", and icon "Success".
-    #>
-
-    param (
-        [string]$ListView,
-        [string]$title = "ITT Emad Adel",
-        [string]$icon = "Info"
-    )
-    switch ($ListView) {
-        "AppsListView" {
-            UpdateUI -Button "InstallBtn" -Content "Install" -Width "140"
-            Notify -title "$title" -msg "All installations have finished" -icon "Info" -time 30000
-            Add-Log -Message "::::All installations have finished::::"
-            Set-Statusbar -Text "📢 All installations have finished"
-        }
-        "TweaksListView" {
-            UpdateUI -Button "ApplyBtn" -Content "Apply" -Width "140"
-            Add-Log -Message "::::All tweaks have finished::::"
-            Set-Statusbar -Text "📢 All tweaks have finished"
-            Notify -title "$title" -msg "All tweaks have finished" -icon "Info" -time 30000
-        }
-    }
-
-    # Reset Taskbar Progress
-    $itt["window"].Dispatcher.Invoke([action] { Set-Taskbar -progress "None" -value 0.01 -icon "done" })
-
-    # Uncheck all items in ListView
-    $itt.$ListView.Dispatcher.Invoke([Action] {
-            # Uncheck all items
-            foreach ($item in $itt.$ListView.Items) {
-                if ($item.Children.Count -gt 0 -and $item.Children[0].Children.Count -gt 0) {
-                    $item.Children[0].Children[0].IsChecked = $false
-                }
-            }
-            
-            # Clear the list view selection and reset the filter
-            $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt.$ListView.Items)
-            $collectionView.Filter = $null
-            $collectionView.Refresh()
-
-            # Close window after install apps
-            # if ($i -ne "") {
-            #     Manage-Music -action "StopAll" 
-            #     $itt["window"].Close()
-            # }
-        })
-}
-function Show-Selected {
-    
-    param (
-        [string]$ListView,
-        [string]$mode
-    )
-
-    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt.$ListView.ItemsSource)
-
-    switch ($mode) {
-        "Filter" {
-            $collectionView.Filter = {
-                param ($item)
-
-                # Check if item is selected
-                return $item.IsChecked -eq $true
-            }
-        }
-        Default {
-
-            $collectionView.Filter = {
-                param ($item)
-
-                # Uncheck all checkboxes
-                $item.IsChecked = $false
-            }
-
-            # Reset collection view
-            $collectionView.Filter = $null
-        }
-    }
-}
 function Get-SelectedItems {
 
     <#
@@ -1120,6 +1035,119 @@ function Install-Winget {
         Write-Error "Failed to install $_"
     }
 }
+function Show-Selected {
+    
+    param (
+        [string]$ListView,
+        [string]$mode
+    )
+
+    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt.$ListView.ItemsSource)
+
+    switch ($mode) {
+        "Filter" {
+            $collectionView.Filter = {
+                param ($item)
+
+                # Check if item is selected
+                return $item.IsChecked -eq $true
+            }
+        }
+        Default {
+
+            $collectionView.Filter = {
+                param ($item)
+
+                # Uncheck all checkboxes
+                $item.IsChecked = $false
+            }
+
+            # Reset collection view
+            $collectionView.Filter = $null
+        }
+    }
+}
+function Finalize {
+    
+    <#
+        .SYNOPSIS
+        Clears checkboxes in a specified ListView and displays a notification.
+        .DESCRIPTION
+        Clears all checkboxes in the ListView named "myListView" and displays a notification with the title "Process Completed", message "All items have been processed", and icon "Success".
+    #>
+
+    param (
+        [string]$ListView,
+        [string]$title = "ITT Emad Adel",
+        [string]$icon = "Info"
+    )
+    switch ($ListView) {
+        "AppsListView" {
+            UpdateUI -Button "InstallBtn" -Content "Install" -Width "140"
+            Notify -title "$title" -msg "All installations have finished" -icon "Info" -time 30000
+            Add-Log -Message "::::All installations have finished::::"
+            Set-Statusbar -Text "📢 All installations have finished"
+        }
+        "TweaksListView" {
+            UpdateUI -Button "ApplyBtn" -Content "Apply" -Width "140"
+            Add-Log -Message "::::All tweaks have finished::::"
+            Set-Statusbar -Text "📢 All tweaks have finished"
+            Notify -title "$title" -msg "All tweaks have finished" -icon "Info" -time 30000
+        }
+    }
+
+    # Reset Taskbar Progress
+    $itt["window"].Dispatcher.Invoke([action] { Set-Taskbar -progress "None" -value 0.01 -icon "done" })
+
+    # Uncheck all items in ListView
+    $itt.$ListView.Dispatcher.Invoke([Action] {
+            # Uncheck all items
+            foreach ($item in $itt.$ListView.Items) {
+                $item.IsChecked = $false
+            }
+            
+            # Clear the list view selection and reset the filter
+            $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt.$ListView.Items)
+            $collectionView.Filter = $null
+            $collectionView.Refresh()
+        })
+}
+
+function Search {
+
+
+    $filter = $itt.searchInput.Text.ToLower() -replace '[^\p{L}\p{N}]', ''
+    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt['window'].FindName($itt.currentList).ItemsSource)
+
+    $collectionView.Filter = {
+        param ($item)
+
+
+        return $item.Name -match $filter -or $item.Category -match $filter
+    }
+
+    $collectionView.Refresh()
+}
+function FilterByCat {
+
+    param ($Cat)
+
+    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt['window'].FindName($itt.CurrentList).ItemsSource)
+
+    if ($Cat -eq "All" -or [string]::IsNullOrWhiteSpace($Cat)) {
+
+        $collectionView.Filter = $null
+    }
+    else {
+        $collectionView.Filter = {
+            param ($item)
+
+            return $item.Category -ieq $Cat
+        }
+    }
+
+    $collectionView.Refresh()
+}
 function Refresh-Explorer {
 
 
@@ -1679,7 +1707,7 @@ function Invoke-Apply {
         }
 
         $itt.ProcessRunning = $false
-        Finish -ListView "TweaksListView"
+        Finalize -ListView "TweaksListView"
     }
 }
 function Invoke-Install {
@@ -1759,8 +1787,8 @@ function Invoke-Install {
             # debug end
         }
 
-        Finish -ListView "AppsListView"
         $itt.ProcessRunning = $false
+        Finalize -ListView "AppsListView"
     }
 }
 function Invoke-Toggle {
@@ -2442,41 +2470,6 @@ function Find-Driver {
         Start-Process "https://www.intel.com/content/www/us/en/search.html?ws=idsa-suggested#q=$encodedName&sort=relevancy&f:@tabfilter=[Downloads]"
     }
     
-}
-function Search {
-
-
-    $filter = $itt.searchInput.Text.ToLower() -replace '[^\p{L}\p{N}]', ''
-    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt['window'].FindName($itt.currentList).ItemsSource)
-
-    $collectionView.Filter = {
-        param ($item)
-
-
-        return $item.Name -match $filter -or $item.Category -match $filter
-    }
-
-    $collectionView.Refresh()
-}
-function FilterByCat {
-
-    param ($Cat)
-
-    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt['window'].FindName($itt.CurrentList).ItemsSource)
-
-    if ($Cat -eq "All" -or [string]::IsNullOrWhiteSpace($Cat)) {
-
-        $collectionView.Filter = $null
-    }
-    else {
-        $collectionView.Filter = {
-            param ($item)
-
-            return $item.Category -ieq $Cat
-        }
-    }
-
-    $collectionView.Refresh()
 }
 $KeyEvents = {
 
@@ -4518,6 +4511,11 @@ function Show-Event {
         $itt.event.FindName('date').text = '06/08/2025'.Trim()
         
     
+            $itt.event.FindName('shell').add_MouseLeftButtonDown({
+                    Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
+                })
+            
+            
             $itt.event.FindName('preview2').add_MouseLeftButtonDown({
                     Start-Process('https://github.com/emadadel4/itt')
                 })
@@ -4530,11 +4528,6 @@ function Show-Event {
             
             $itt.event.FindName('preview').add_MouseLeftButtonDown({
                     Start-Process('https://github.com/emadadel4/itt')
-                })
-            
-            
-            $itt.event.FindName('shell').add_MouseLeftButtonDown({
-                    Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
                 })
             
             
@@ -4829,7 +4822,7 @@ $InitialSessionState.Variables.Add($hashVars)
 
 $functions = @(
     'Install-App','Install-Dependencies','InvokeCommand', 'Add-Log',
-    'Disable-Service', 'Uninstall-AppxPackage', 'Finish', 'Message',
+    'Disable-Service', 'Uninstall-AppxPackage', 'Finalize', 'Message',
     'Notify','UpdateUI', 'ExecuteCommand', 'Set-Registry', 'Set-Taskbar',
     'Refresh-Explorer','Remove-ScheduledTasks','CreateRestorePoint','Set-Statusbar'
 )
