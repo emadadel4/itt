cbae409f7b355703db530d0d355d733d801f2229
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-08-01 13:55:18 +0300

    update

--- scripts/UI/About-Dev.ps1
+++ scripts/UI/About-Dev.ps1
@@ -3,6 +3,5 @@ function About {
     $aboutPopup.FindName('ver').Text = "Last update $($itt.lastupdate)"
     $aboutPopup.FindName("telegram").Add_MouseLeftButtonDown({ Start-Process("https://t.me/emadadel4") })
     $aboutPopup.FindName("github").Add_MouseLeftButtonDown({ Start-Process("https://github.com/emadadel4/itt") })
-    $aboutPopup.FindName("blog").Add_MouseLeftButtonDown({ Start-Process("https://emadadel4.github.io") })
     $aboutPopup.IsOpen = $true
 }
