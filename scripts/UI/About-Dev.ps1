63d3816a83b2127f78d33495a5ff793f988de726
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-08-01 14:19:04 +0300

    Update About-Dev.ps1

--- scripts/UI/About-Dev.ps1
+++ scripts/UI/About-Dev.ps1
@@ -1,7 +1,5 @@
 function About {
     $aboutPopup = $itt['window'].FindName('AboutPopup')
     $aboutPopup.FindName('ver').Text = "Last update $($itt.lastupdate)"
-    $aboutPopup.FindName("telegram").Add_MouseLeftButtonDown({ Start-Process("https://t.me/emadadel4") })
-    $aboutPopup.FindName("github").Add_MouseLeftButtonDown({ Start-Process("https://github.com/emadadel4/itt") })
     $aboutPopup.IsOpen = $true
 }
