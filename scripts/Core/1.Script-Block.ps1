9637b68135f7b9838579e79a47ccebc50fb6da6f
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-08-13 09:55:12 +0300

    Update 1.Script-Block.ps1

--- scripts/Core/1.Script-Block.ps1
+++ scripts/Core/1.Script-Block.ps1
@@ -1,26 +1,27 @@
 function ITT-ScriptBlock {
+    
     <#
         .SYNOPSIS
         Executes a given script block asynchronously within a specified runspace.
-        .DESCRIPTION
-        This function creates a new PowerShell instance to execute a provided script block asynchronously. It accepts an optional array of arguments to pass to the script block and manages the runspace and PowerShell instance resources. The function ensures that resources are properly disposed of after the script block completes execution.
-        .EXAMPLE
-        ITT-ScriptBlock -ScriptBlock { param($arg1) Write-Output $arg1 } -ArgumentList @("Hello, World!")
-        Executes the script block that outputs the provided argument "Hello, World!" asynchronously.
     #>
+    
     param(
         [scriptblock]$ScriptBlock,
         [array]$ArgumentList,
         $Debug
     )
+
     $script:powershell = [powershell]::Create()
+
     # Add the script block and arguments to the runspace
     $script:powershell.AddScript($ScriptBlock)
     $script:powershell.AddArgument($ArgumentList)
     $script:powershell.AddArgument($Debug)
     $script:powershell.RunspacePool = $itt.runspace
+
     # Begin running the script block asynchronously
     $script:handle = $script:powershell.BeginInvoke()
+
     # If the script has completed, clean up resources
     if ($script:handle.IsCompleted) {
         $script:powershell.EndInvoke($script:handle)
