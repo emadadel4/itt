2fdaa9c84141cce9723c973d1331d6b3bc177331
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-08-15 22:53:04 +0300

    Update ExecuteCommand.ps1

--- scripts/Core/ExecuteCommand.ps1
+++ scripts/Core/ExecuteCommand.ps1
@@ -3,21 +3,14 @@ function ExecuteCommand {
     <#
         .SYNOPSIS
         Executes a PowerShell command in a new process.
-        .DESCRIPTION
-        This function starts a new PowerShell process to execute the specified command.
-        .EXAMPLE
-        ExecuteCommand -Name "Greeting" -Command "Write-Output 'Welcome to ITT'"
-        Executes the PowerShell command `Write-Output 'Welcome to ITT'` in a new PowerShell process.
     #>
 
-    param ([array]$tweak)
+    param ($tweak)
 
     try {
-        foreach ($cmd in $tweak) {
-            Add-Log -Message "Please wait..."
-            $script = [scriptblock]::Create($cmd)
-            Invoke-Command  $script -ErrorAction Stop
-        }
+        Add-Log -Message "Please wait..."
+        $script = [scriptblock]::Create($tweak)
+        Invoke-Command  $script -ErrorAction Stop
     } catch  {
         Add-Log -Message "The specified command was not found." -Level "WARNING"
     }
