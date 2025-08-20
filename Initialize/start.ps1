15955312e703a98e279a71ba1b9bc9f0bfe41651
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-08-20 13:22:33 +0300

    Update start.ps1

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -6,7 +6,7 @@ param (
     [string]$i
 )
 
-Write-Host "Relax, good things are loading… almost there!" -ForegroundColor Yellow
+Write-Host "`n  Relax, good things are loading… almost there!" -ForegroundColor Yellow
 
 # Load DLLs
 Add-Type -AssemblyName 'System.Windows.Forms', 'PresentationFramework', 'PresentationCore', 'WindowsBase','System.Net.Http'
