63affa48d7a10d2ff7f7bc193a444ca58461f74d
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-09-01 23:51:54 +0300

    Update Startup-Log.ps1

--- scripts/Core/Startup-Log.ps1
+++ scripts/Core/Startup-Log.ps1
@@ -24,7 +24,7 @@ function Startup {
             }
         }
         function PlayMusic {
-            $tracks = (Invoke-RestMethod "https://raw.githubusercontent.com/emadadel4/itt/refs/heads/main/static/Database/birthdayplaylist.m3u") -split "`n" | Where-Object { $_ -and ($_ -notmatch "^#") }
+            $tracks = (Invoke-RestMethod "https://raw.githubusercontent.com/emadadel4/itt/refs/heads/main/static/Database/ittplaylist.m3u") -split "`n" | Where-Object { $_ -and ($_ -notmatch "^#") }
             $shuffledTracks = $tracks | Get-Random -Count $tracks.Count
             foreach ($track in $shuffledTracks) {
                 $mediaItem = $itt.mediaPlayer.newMedia($track)
