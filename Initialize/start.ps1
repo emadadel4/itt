80278dd350bb8f98bbea2cc346e4de8ba916020f
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-09-01 05:27:12 +0300

    Update start.ps1

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -28,6 +28,11 @@ $itt = [Hashtable]::Synchronized(@{
     command        = "$($MyInvocation.MyCommand.Definition)"
 })
 
+# Check for updates
+$repoFileUrl = "https://api.github.com/repos/emadadel4/itt/releases/latest"
+$latestVersion = (Invoke-RestMethod -Uri $repoFileUrl).tag_name
+if ($latestVersion -ne $itt.lastupdate) {Write-Host "Your using an old version of ITT, Portable script is not Recommended`n Please use latest version of ITT from https://github.com/emadadel4/itt" -ForegroundColor Red}
+
 # Ask user for administrator privileges if not already running as admin
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process -FilePath "PowerShell" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$($MyInvocation.MyCommand.Definition)`"" -Verb RunAs
