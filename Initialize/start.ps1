0a6558a13e7a19cf8a755264446d741ed3d7fe60
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-09-07 07:49:12 +0300

    Update start.ps1

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -6,11 +6,14 @@ param (
     [string]$i
 )
 
-$Host.UI.RawUI.WindowTitle = "Install Twaeks Tool"
-
 # Load DLLs
 Add-Type -AssemblyName 'System.Windows.Forms', 'PresentationFramework', 'PresentationCore', 'WindowsBase','System.Net.Http'
+$Host.UI.RawUI.WindowTitle = "Install Twaeks Tool"
+$checkUrl = "https://debug.emadadel4.workers.dev/check?version=$($itt.lastupdate)"
 
+# ================================
+#region Hashtable
+# ================================
 # Synchronized Hashtable for shared variables
 $itt = [Hashtable]::Synchronized(@{
 
@@ -27,22 +30,35 @@ $itt = [Hashtable]::Synchronized(@{
     ittDir         = "$env:ProgramData\itt\"
     command        = "$($MyInvocation.MyCommand.Definition)"
 })
+# ================================
+#endregion Hashtable
+# ================================
 
-# Check for updates
-$repoFileUrl = "https://api.github.com/repos/emadadel4/itt/releases/latest"
-$latestVersion = (Invoke-RestMethod -Uri $repoFileUrl).tag_name
-if ($latestVersion -ne $itt.lastupdate) {
-    Write-Host "`n  YOU ARE USING AN OLD VERSION OF ITT. PORTABLE SCRIPT IS NOT RECOMMENDED.`n  PLEASE USE THE LATEST VERSION FROM THE OFFICIAL COMMANDS AT https://github.com/emadadel4/itt" -ForegroundColor Red
-    Read-Host -Prompt "Press any key to continue..."
+# ================================
+#region Check for updates
+# ================================
+$response = Invoke-RestMethod -Uri $checkUrl -ErrorAction Stop
+if ($response.status) {
+    Write-Host "$($response.message)" -ForegroundColor Red
+    read-host "   Press Enter to exit..."
     Start-Process("https://github.com/emadadel4/itt")
     exit
 }
+# ================================
+#endregion Check for updates
+# ================================
 
+# ================================
+#region Ask user for administrator privileges if not already running as admin
+# ================================
 # Ask user for administrator privileges if not already running as admin
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process -FilePath "PowerShell" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$($MyInvocation.MyCommand.Definition)`"" -Verb RunAs
     exit
 }
+# ================================
+#endregion Ask user for administrator privileges if not already running as admin
+# ================================
 
 Write-Host "`n  Relax, good things are loading… almost there!" -ForegroundColor Yellow
 
