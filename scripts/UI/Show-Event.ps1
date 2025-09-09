87b1cc63a84c395b0a618e4336d34f687441a875
itt-co <19177373+emadadel4@users.noreply.github.com>
2025-09-09 11:09:00 +0300

    Update Show-Event.ps1

--- scripts/UI/Show-Event.ps1
+++ scripts/UI/Show-Event.ps1
@@ -1,21 +1,13 @@
 function Show-Event {
 
-    $itt['window'].FindName('DisablePopup').add_MouseLeftButtonDown({
-        Set-ItemProperty -Path $itt.registryPath -Name "PopupWindow" -Value 1 -Force
-        $popup.IsOpen = $false
-    })
-
     #{title}
     #{contorlshandler}
 
     $storedDate = [datetime]::ParseExact($itt['window'].FindName('date').Text, 'MM/dd/yyyy', $null)
     $daysElapsed = (Get-Date) - $storedDate
 
-    if (($daysElapsed.Days -ge 1) -and (($itt.PopupWindow -ne "0") -or $i)) { return }
-
-    if ($daysElapsed.Days -lt 1) {
-        $itt['window'].FindName('DisablePopup').Visibility = 'Hidden'
-    }
-
-    $popup.IsOpen = $true
+    if ($daysElapsed.Days -lt 1) 
+    {
+        $itt['window'].FindName('hotdot').Visibility = [System.Windows.Visibility]::Visible
+    } 
 }
